6b02c5b28975ad6cbd2409b87012e9d1ecb02783
Ingmars Melkis <idmelkis@edu.riga.lv>
2023-02-03 09:50:52 +0200

    Pārtaisījām par Wordle

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
 import random
+from colorama import Fore, Back, Style
+
 
 words = [ "viens", "divi", "tris", "cits" ]
-inGame = False
-totalLives = 5
-word = random.choice(words)
-guessedWord = list("_" for _ in word) # ["_", "_", "_"]
-print(word)
-#print("".join(guessedWord))
-#for _ in word:
-    #guessedWord += "_"
-while (totalLives > 0 and not "".join(guessedWord) == word):  
-    inp = input("Ievade (burts): ")
-    if (len(inp) < 1): continue
-    inp = inp[0]
 
-    didGuessLetter = False
-    for aaa in range(0,  len(guessedWord)):
-        if word[aaa] == inp:
-            guessedWord[aaa] = inp
-            didGuessLetter = True
+while True:
+    totalLives = 5
+    word = random.choice(words)
+    guessedWord = list("_" for _ in word) # ["_", "_", "_"]
+    print(word)
+    #print("".join(guessedWord))
+    #for _ in word:
+        #guessedWord += "_"
+    while (totalLives > 0 and not "".join(guessedWord) == word):  
+        inp = input("Ievade (vārds): ")
+        if (len(inp) != len(word)): continue
+        #inp = inp[0]
 
-    if not didGuessLetter:
+        #print(f"{Fore.GREEN}T{Style.RESET_ALL}eksts")
+        #print(Style.RESET_ALL)
+        guessedWord = list("_" for _ in word)
+        for aaa in range(0,  len(word)):
+            if word[aaa] == inp[aaa]:
+                guessedWord[aaa] = inp[aaa]
+                print(f"{Fore.GREEN}{inp[aaa]}{Style.RESET_ALL}", end="")
+            elif inp[aaa] in word:
+                print(f"{Fore.YELLOW}{inp[aaa]}{Style.RESET_ALL}", end="")
+            else:
+                print(f"{inp[aaa]}", end="")
+        print("")
         totalLives -= 1
 
-    #print(didGuessLetter)
-    print("".join(guessedWord))
+        #print(didGuessLetter)
+        #print("".join(guessedWord))
 
-if (totalLives > 0):
-    print("Uzvara")
-else:
-    print("Zaudēja")
+    if (totalLives > 0):
+        print("Uzvara")
+    else:
+        print("Zaudēja")
+
+    if input("Vai beigt spēli vai nē? (Y/N)")[0].lower() == "y":
+        break
