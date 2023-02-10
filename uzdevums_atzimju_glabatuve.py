2d2f33049751e93e430263ceeeeddc16effb852d
Ingmars Melkis <idmelkis@edu.riga.lv>
2023-02-10 09:38:36 +0200

    Pievienoju uzdevuma nosacījumus atzīmju db programmai

---
+++ uzdevums_atzimju_glabatuve.py
@@ -0,0 +1,16 @@
+#Programma, kas ļauj ievadīt un saglabāt priekšmetu atzīmes
+
+#1. Jāļauj ievadīt priekšmetus (input)
+#2. Jāizvada ievadītie priekšmeti (print, for)
+#3. Jāļauj ievadīt priekšmetu atzīmes (input) -- 1 - 10, nv
+#4. Jāļauj lietotājam izvēlēties priekšmetu un izvadīt atzīmes saistītas ar šo priekšmetu (...)
+#5. Jānodrošina priekšmetu un atzīmju saglabāšana failā (json.dumps)
+#6. Jānodrošina priekšmetu un atzīmju ielāde no faila (json.load)
+#7. Jānodrošina priekšmetu un atzīmju dzēšana
+# Atzīmes var glabāt
+class prieksmets:
+    nosaukums = ""
+    atzimes = []
+# vai
+atzimes = { "prieksmets1" : [] }
+atzimes["citsprieksmets"] = []
